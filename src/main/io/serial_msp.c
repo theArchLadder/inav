betaflight/betaflight

config: pull boardAlignment out as a PG example.
Add PG based MSP get and set support.

Shows moving boardAlignment out of masterConfig, adding it to the
registry, and how there's no MSP code needed.

Signed-off-by: Michael Hope <mlhx@google.com>

src/main/io/serial_msp.c
@@ -78,6 +78,7 @@
 #include "config/config.h"
 #include "config/config_profile.h"
 #include "config/config_master.h"
+#include "config/parameter_group.h"
 
 #include "version.h"
 #ifdef NAZE
@@ -521,6 +522,13 @@ static bool processOutCommand(uint8_t cmdMSP)
     uint8_t wp_no;
     int32_t lat = 0, lon = 0;
 #endif
+    const pgRegistry_t *reg = pgFind(cmdMSP);
+
+    if (reg != NULL) {
+        headSerialReply(reg->size);
+        s_struct(reg->base, reg->size);
+        return true;
+    }
 
     switch (cmdMSP) {
     case MSP_API_VERSION:
@@ -922,13 +930,6 @@ static bool processOutCommand(uint8_t cmdMSP)
         serialize32(featureMask());
         break;
 
-    case MSP_BOARD_ALIGNMENT:
-        headSerialReply(6);
-        serialize16(masterConfig.boardAlignment.rollDegrees);
-        serialize16(masterConfig.boardAlignment.pitchDegrees);
-        serialize16(masterConfig.boardAlignment.yawDegrees);
-        break;
-
     case MSP_VOLTAGE_METER_CONFIG:
         headSerialReply(4);
         serialize8(masterConfig.batteryConfig.vbatscale);
@@ -987,9 +988,9 @@ static bool processOutCommand(uint8_t cmdMSP)
 
         serialize8(masterConfig.rxConfig.serialrx_provider);
 
-        serialize16(masterConfig.boardAlignment.rollDegrees);
-        serialize16(masterConfig.boardAlignment.pitchDegrees);
-        serialize16(masterConfig.boardAlignment.yawDegrees);
+        serialize16(boardAlignment.rollDegrees);
+        serialize16(boardAlignment.pitchDegrees);
+        serialize16(boardAlignment.yawDegrees);
 
         serialize16(masterConfig.batteryConfig.currentMeterScale);
         serialize16(masterConfig.batteryConfig.currentMeterOffset);
@@ -1073,6 +1074,13 @@ static bool processInCommand(void)
     uint8_t wp_no;
     int32_t lat = 0, lon = 0, alt = 0;
 #endif
+
+    const pgRegistry_t *reg = pgFindForSet(currentPort->cmdMSP);
+
+    if (reg != NULL) {
+        pgLoad(reg, currentPort->inBuf + currentPort->indRX, currentPort->dataSize);
+        return true;
+    }
 
     switch (currentPort->cmdMSP) {
     case MSP_SELECT_SETTING:
@@ -1342,12 +1350,6 @@ static bool processInCommand(void)
         featureSet(read32()); // features bitmap
         break;
 
-    case MSP_SET_BOARD_ALIGNMENT:
-        masterConfig.boardAlignment.rollDegrees = read16();
-        masterConfig.boardAlignment.pitchDegrees = read16();
-        masterConfig.boardAlignment.yawDegrees = read16();
-        break;
-
     case MSP_SET_VOLTAGE_METER_CONFIG:
         masterConfig.batteryConfig.vbatscale = read8();           // actual vbatscale as intended
         masterConfig.batteryConfig.vbatmincellvoltage = read8();  // vbatlevel_warn1 in MWC2.3 GUI
@@ -1409,9 +1411,9 @@ static bool processInCommand(void)
 
         masterConfig.rxConfig.serialrx_provider = read8(); // serialrx_type
 
-        masterConfig.boardAlignment.rollDegrees = read16(); // board_align_roll
-        masterConfig.boardAlignment.pitchDegrees = read16(); // board_align_pitch
-        masterConfig.boardAlignment.yawDegrees = read16(); // board_align_yaw
+        boardAlignment.rollDegrees = read16(); // board_align_roll
+        boardAlignment.pitchDegrees = read16(); // board_align_pitch
+        boardAlignment.yawDegrees = read16(); // board_align_yaw
 
         masterConfig.batteryConfig.currentMeterScale = read16();
         masterConfig.batteryConfig.currentMeterOffset = read16();

src/main/sensors/boardalignment.c
@@ -22,10 +22,21 @@
 
 #include "common/maths.h"
 #include "common/axis.h"
+#include "io/msp_protocol.h"
 
 #include "sensors.h"
 
 #include "boardalignment.h"
+
+boardAlignment_t boardAlignment;
+
+static const pgRegistry_t boardAlignmentRegistry PG_REGISTRY_SECTION =
+{
+    .base = &boardAlignment,
+    .size = sizeof(boardAlignment),
+    .pgn = MSP_BOARD_ALIGNMENT,
+    .pgn_for_set = MSP_SET_BOARD_ALIGNMENT,
+} ;
 
 static bool standardBoardAlignment = true;     // board orientation correction
 static float boardRotation[3][3];              // matrix
@@ -35,18 +46,18 @@ static bool isBoardAlignmentStandard(boardAlignment_t *boardAlignment)
     return !boardAlignment->rollDegrees && !boardAlignment->pitchDegrees && !boardAlignment->yawDegrees;
 }
 
-void initBoardAlignment(boardAlignment_t *boardAlignment)
+void initBoardAlignment()
 {
-    if (isBoardAlignmentStandard(boardAlignment)) {
+    if (isBoardAlignmentStandard(&boardAlignment)) {
         return;
     }
 
     standardBoardAlignment = false;
 
     fp_angles_t rotationAngles;
-    rotationAngles.angles.roll = degreesToRadians(boardAlignment->rollDegrees);
-    rotationAngles.angles.pitch = degreesToRadians(boardAlignment->pitchDegrees);
-    rotationAngles.angles.yaw = degreesToRadians(boardAlignment->yawDegrees);
+    rotationAngles.angles.roll = degreesToRadians(boardAlignment.rollDegrees);
+    rotationAngles.angles.pitch = degreesToRadians(boardAlignment.pitchDegrees);
+    rotationAngles.angles.yaw = degreesToRadians(boardAlignment.yawDegrees);
 
     buildRotationMatrix(&rotationAngles, boardRotation);
 }

theArchLadder/inav

src/main/io/serial_msp.c
@@ -80,6 +80,7 @@
 #include "config/config.h"
 #include "config/config_profile.h"
 #include "config/config_master.h"
+#include "config/parameter_group.h"
 
 #include "version.h"
 #ifdef NAZE
@@ -587,6 +588,13 @@ static bool processOutCommand(uint8_t cmdMSP)
     int8_t msp_wp_no;
     navWaypoint_t msp_wp;
 #endif
+    const pgRegistry_t *reg = pgFind(cmdMSP);
+
+    if (reg != NULL) {
+        headSerialReply(reg->size);
+        s_struct(reg->base, reg->size);
+        return true;
+    }
 
     switch (cmdMSP) {
     case MSP_API_VERSION:
@@ -986,13 +994,6 @@ static bool processOutCommand(uint8_t cmdMSP)
         serialize32(featureMask());
         break;
 
-    case MSP_BOARD_ALIGNMENT:
-        headSerialReply(6);
-        serialize16(masterConfig.boardAlignment.rollDeciDegrees);
-        serialize16(masterConfig.boardAlignment.pitchDeciDegrees);
-        serialize16(masterConfig.boardAlignment.yawDeciDegrees);
-        break;
-
     case MSP_VOLTAGE_METER_CONFIG:
         headSerialReply(4);
         serialize8(masterConfig.batteryConfig.vbatscale);
@@ -1062,9 +1063,9 @@ static bool processOutCommand(uint8_t cmdMSP)
 
         serialize8(masterConfig.rxConfig.serialrx_provider);
 
-        serialize16(masterConfig.boardAlignment.rollDeciDegrees);
-        serialize16(masterConfig.boardAlignment.pitchDeciDegrees);
-        serialize16(masterConfig.boardAlignment.yawDeciDegrees);
+        serialize16(boardAlignment.rollDeciDegrees);
+        serialize16(boardAlignment.pitchDeciDegrees);
+        serialize16(boardAlignment.yawDeciDegrees);
 
         serialize16(masterConfig.batteryConfig.currentMeterScale);
         serialize16(masterConfig.batteryConfig.currentMeterOffset);
@@ -1170,6 +1171,13 @@ static bool processInCommand(void)
     uint8_t msp_wp_no;
     navWaypoint_t msp_wp;
 #endif
+
+    const pgRegistry_t *reg = pgFindForSet(currentPort->cmdMSP);
+
+    if (reg != NULL) {
+        pgLoad(reg, currentPort->inBuf + currentPort->indRX, currentPort->dataSize);
+        return true;
+    }
 
     switch (currentPort->cmdMSP) {
 #ifdef HIL
@@ -1461,12 +1469,6 @@ static bool processInCommand(void)
         featureSet(read32()); // features bitmap
         break;
 
-    case MSP_SET_BOARD_ALIGNMENT:
-        masterConfig.boardAlignment.rollDeciDegrees = read16();
-        masterConfig.boardAlignment.pitchDeciDegrees = read16();
-        masterConfig.boardAlignment.yawDeciDegrees = read16();
-        break;
-
     case MSP_SET_VOLTAGE_METER_CONFIG:
         masterConfig.batteryConfig.vbatscale = read8();           // actual vbatscale as intended
         masterConfig.batteryConfig.vbatmincellvoltage = read8();  // vbatlevel_warn1 in MWC2.3 GUI
@@ -1541,9 +1543,9 @@ static bool processInCommand(void)
 
         masterConfig.rxConfig.serialrx_provider = read8(); // serialrx_type
 
-        masterConfig.boardAlignment.rollDeciDegrees = read16(); // board_align_roll
-        masterConfig.boardAlignment.pitchDeciDegrees = read16(); // board_align_pitch
-        masterConfig.boardAlignment.yawDeciDegrees = read16(); // board_align_yaw
+        boardAlignment.rollDeciDegrees = read16(); // board_align_roll
+        boardAlignment.pitchDeciDegrees = read16(); // board_align_pitch
+        boardAlignment.yawDeciDegrees = read16(); // board_align_yaw
 
         masterConfig.batteryConfig.currentMeterScale = read16();
         masterConfig.batteryConfig.currentMeterOffset = read16();

src/main/sensors/boardalignment.c
@@ -22,10 +22,21 @@
 
 #include "common/maths.h"
 #include "common/axis.h"
+#include "io/msp_protocol.h"
 
 #include "sensors.h"
 
 #include "boardalignment.h"
+
+boardAlignment_t boardAlignment;
+
+static const pgRegistry_t boardAlignmentRegistry PG_REGISTRY_SECTION =
+{
+    .base = &boardAlignment,
+    .size = sizeof(boardAlignment),
+    .pgn = MSP_BOARD_ALIGNMENT,
+    .pgn_for_set = MSP_SET_BOARD_ALIGNMENT,
+} ;
 
 static bool standardBoardAlignment = true;     // board orientation correction
 static float boardRotation[3][3];              // matrix
@@ -35,9 +46,9 @@ static bool isBoardAlignmentStandard(boardAlignment_t *boardAlignment)
     return !boardAlignment->rollDeciDegrees && !boardAlignment->pitchDeciDegrees && !boardAlignment->yawDeciDegrees;
 }
 
-void initBoardAlignment(boardAlignment_t *boardAlignment)
+void initBoardAlignment()
 {
-    if (isBoardAlignmentStandard(boardAlignment)) {
+    if (isBoardAlignmentStandard(&boardAlignment)) {
         standardBoardAlignment = true;
     }
     else {
@@ -45,23 +56,23 @@ void initBoardAlignment(boardAlignment_t *boardAlignment)
 
         standardBoardAlignment = false;
 
-        rotationAngles.angles.roll  = DECIDEGREES_TO_RADIANS(boardAlignment->rollDeciDegrees );
-        rotationAngles.angles.pitch = DECIDEGREES_TO_RADIANS(boardAlignment->pitchDeciDegrees);
-        rotationAngles.angles.yaw   = DECIDEGREES_TO_RADIANS(boardAlignment->yawDeciDegrees  );
+        rotationAngles.angles.roll  = DECIDEGREES_TO_RADIANS(boardAlignment.rollDeciDegrees);
+        rotationAngles.angles.pitch = DECIDEGREES_TO_RADIANS(boardAlignment.pitchDeciDegrees);
+        rotationAngles.angles.yaw   = DECIDEGREES_TO_RADIANS(boardAlignment.yawDeciDegrees);
 
         buildRotationMatrix(&rotationAngles, boardRotation);
     }
 }
 
-void updateBoardAlignment(boardAlignment_t *boardAlignment, int16_t roll, int16_t pitch)
+void updateBoardAlignment(int16_t roll, int16_t pitch)
 {
-    float sinAlignYaw = sin_approx(DECIDEGREES_TO_RADIANS(boardAlignment->yawDeciDegrees));
-    float cosAlignYaw = cos_approx(DECIDEGREES_TO_RADIANS(boardAlignment->yawDeciDegrees));
+    float sinAlignYaw = sin_approx(DECIDEGREES_TO_RADIANS(boardAlignment.yawDeciDegrees));
+    float cosAlignYaw = cos_approx(DECIDEGREES_TO_RADIANS(boardAlignment.yawDeciDegrees));
 
-    boardAlignment->rollDeciDegrees += -sinAlignYaw * pitch + cosAlignYaw * roll;
-    boardAlignment->pitchDeciDegrees += cosAlignYaw * pitch + sinAlignYaw * roll;
+    boardAlignment.rollDeciDegrees += -sinAlignYaw * pitch + cosAlignYaw * roll;
+    boardAlignment.pitchDeciDegrees += cosAlignYaw * pitch + sinAlignYaw * roll;
 
-    initBoardAlignment(boardAlignment);
+    initBoardAlignment();
 }
 
 static void alignBoard(int32_t *vec)